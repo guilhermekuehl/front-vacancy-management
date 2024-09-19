146d3d233ddd53471a17719d1aa5fbb6e75a8bd9
guilhermekuehl <guikuehlkeller120@gmail.com>
2024-09-18 22:34:54 -0300

    feat: insert profile datas

--- src/main/java/br/com/guilhermekellermann/front_gestao_vagas/modules/candidate/controller/CandidateController.java
+++ src/main/java/br/com/guilhermekellermann/front_gestao_vagas/modules/candidate/controller/CandidateController.java
@@ -12,6 +12,7 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.client.HttpClientErrorException;
@@ -62,10 +63,12 @@ public class CandidateController {
 
     @GetMapping("/profile")
     @PreAuthorize("hasRole('CANDIDATE')")
-    public String profile() {
+    public String profile(Model model) {
 
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        var result = this.profileCandidateService.execute(authentication.getDetails().toString());
+        var user = this.profileCandidateService.execute(authentication.getDetails().toString());
+
+        model.addAttribute("user", user);
 
         return "candidate/profile";
     }
